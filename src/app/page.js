e9a303872f641eb1fd72a65756cd5bcbe753037e
unknown <carlos.barreraacuna@gmail.com>
2024-04-14 01:07:05 -0500

    Ultimos cambios

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 // export default function Home() {
 //   return (
 //     <main className="flex flex-col items-center justify-between min-h-screen p-24">
@@ -11,30 +11,49 @@ import {motion} from "framer-motion";
 // }
 
 const Home = () => {
-  return ( 
-  <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
-    
-  <div className="flex flex-col h-full px-4 text-xl lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
-  {/* IMAGE CONTAINER */ }
-  <div className="relative h-1/2 lg:h-full lg:w-1/2">
-    <Image src="/perfil.png" alt="" fill className="object-contain"/>
-  </div>
-  {/* TEXT CONTAINER */ }
-  <div className="flex flex-col items-center justify-center gap-8 lg:h-full lg:w-1/2 h-1/2">
-    {/* TITULO */ }
-    <hi className="text-4xl font-bold text-black md:text-6xl">INGENIERO DE SISTEMAS</hi>
-    {/* DESCRIPCION */ }
-    <p className="text-black md:text-xl">Soy Desarrollador de Software. Utilizo mis habilidades técnicas para crear software que cumpla con las necesidades específicas de los usuarios. Trabajo en estrecha colaboración con los clientes para entender sus objetivos y traducirlos en código, creando aplicaciones empresariales que son tanto funcionales como amigables para el usuario.</p>
-     {/* BOTONES */ }
-    <div className="flex w-full gap-4">
-      <a href="/Biografia" className="p-4 text-white bg-black rounded-lg ring-1 ring-black">Conoceme</a>
-      <a href="/contacto" className="p-4 text-black rounded-lg ring-1 ring-black">Contactame</a>
-    </div>
-  </div>
-  </div>
-  </motion.div>
-)}
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="flex flex-col h-full px-4 text-xl lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* IMAGE CONTAINER */}
+        <div className="relative h-1/2 lg:h-full lg:w-1/2">
+          <Image src="/perfil.png" alt="" fill className="object-contain" />
+        </div>
+        {/* TEXT CONTAINER */}
+        <div className="flex flex-col items-center justify-center gap-8 lg:h-full lg:w-1/2 h-1/2">
+          {/* TITULO */}
+          <hi className="text-4xl font-bold text-black md:text-6xl">
+            INGENIERO DE SISTEMAS
+          </hi>
+          {/* DESCRIPCION */}
+          <p className="text-black md:text-xl">
+            Soy un ingeniero de sistemas enfocado en el desarrollo de software,
+            poseo amplia experiencia en el desarrollo de soluciones innovadoras
+            orientadas a la eficiencia y usabilidad.
+          </p>
+          {/* BOTONES */}
+          <div className="flex w-full gap-4">
+            <a
+              href="/Biografia"
+              className="p-4 text-white bg-black rounded-lg ring-1 ring-black"
+            >
+              Conoceme
+            </a>
+            <a
+              href="/contacto"
+              className="p-4 text-black rounded-lg ring-1 ring-black"
+            >
+              Contactame
+            </a>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Home ; 
-
-
+export default Home;
