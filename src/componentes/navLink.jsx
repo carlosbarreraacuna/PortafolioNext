11a1bdfc7e16d96be3f0da859bbf66abed71118b
unknown <carlos.barreraacuna@gmail.com>
2024-05-11 10:12:52 -0500

    Cambiar seleccion sombreada en el navbar por la barra inferior

--- src/componentes/navLink.jsx
+++ src/componentes/navLink.jsx
@@ -5,7 +5,7 @@ const NavLink = ({ link }) => {
     const pathName = usePathname();
     console.log(pathName);
     return (
-        <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>{link.title}</Link>
+        <Link className={` ${pathName === link.url && "border-b-2 border-black"}`} href={link.url}>{link.title}</Link>
     );
 };
 
